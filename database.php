1ba4428445615ed8f1a6b7e029173842e3133805
Mahesh Pradeep Abekoon <maheshpradeepabekoon@gmail.com>
2025-05-06 15:55:08 +0530

    database access update for local test

--- database.php
+++ database.php
@@ -1,8 +1,8 @@
 <?php
-$host = '';
-$dbname = '';
-$user = '';  // Make sure the server name matches here
-$pass = '';
+$host = 'skillsurgedbserver.mysql.database.azure.com';
+$dbname = 'freelance_db';
+$user = 'freelance_user';  // Make sure the server name matches here
+$pass = 'skillsurge123!';
 
 $conn = mysqli_connect($host, $user, $pass, $dbname);
 
