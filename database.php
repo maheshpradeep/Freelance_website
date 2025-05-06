27bc25405188d8c52f079b20315665c038759a46
Mahesh Pradeep Abekoon <maheshpradeepabekoon@gmail.com>
2025-05-06 12:28:53 +0530

    database access update

--- database.php
+++ database.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'db';  // Must match service name in docker-compose.yml
+$host = 'skillsurgedbserver.mysql.database.azure.com';  // Must match service name in docker-compose.yml
 $dbname = 'freelance_db';
-$user = 'freelance_user';
-$pass = 'freelance_pass';
+$user = 'freelance_user@skillsurgedbserver';
+$pass = 'skillsurge123!';
 
 $conn = mysqli_connect($host, $user, $pass, $dbname);
 
