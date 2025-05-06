0587a3786d49bbf0ff96bc3fe0605117c6c5851e
Mahesh Pradeep Abekoon <maheshpradeepabekoon@gmail.com>
2025-05-07 00:05:30 +0530

    final test

--- index.php
+++ index.php
@@ -74,12 +74,6 @@ include('includes/header.php');
 
     <p>This is the test for the check update</p>
     
-    <p>This is the test for the check 2nd update</p>
-    <p>This is the test for the check 3rd update</p>
-    <p>This is the test for the check 4th update</p>
-    <p>This is the test for the check 5th update</p>
-    <p>This is the test for the check 6th update</p>
-    <p>This is the test for the check 7th update</p>
     <?php if (!isset($_SESSION['user_id'])): ?>
       <a href="signup.php" class="btn">Join Now</a>
     <?php endif; ?>
